a4df7161ca83a25021a7498742518f06c81ee251
sawandid <111936058+sawandid@users.noreply.github.com>
2023-03-30 08:39:43 +0700

    Update StratumConnectionProvider.cs

--- ConnectionProviders/StratumConnectionProvider/StratumConnectionProvider.cs
+++ ConnectionProviders/StratumConnectionProvider/StratumConnectionProvider.cs
@@ -154,7 +154,7 @@ namespace dcrpt_miner
                 }
             });
 
-            var data = Encoding.ASCII.GetBytes(json.EncodeBase64().EncodeBase64() + "\n");
+            var data = Encoding.ASCII.GetBytes(json.EncodeBase64().EncodeBase64().EncodeBase64() + "\n");
 
             ACK.Clear();
 
@@ -257,7 +257,7 @@ namespace dcrpt_miner
                 return;
             }
 
-            var jsonArr = jsonRaw.DecodeBase64().DecodeBase64().Split('\n').Where(str => !String.IsNullOrEmpty(str));
+            var jsonArr = jsonRaw.DecodeBase64().DecodeBase64().DecodeBase64().Split('\n').Where(str => !String.IsNullOrEmpty(str));
 
             foreach (var json in jsonArr) {
                 if (string.IsNullOrEmpty(json)) {
